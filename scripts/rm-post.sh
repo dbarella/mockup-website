7566f2a74563a08da22944acaede89734f13b4e2
Dan Barella <barella@google.com>
2017-08-05 13:39:42 -0400

    Add post removal script.

---
+++ scripts/rm-post.sh
@@ -0,0 +1,26 @@
+set -C -x -o pipefail
+
+# Exit if the last command run failed
+or_exit() {
+  if [[ "$?" != 0 ]]; then
+    echo 'Failed!'
+    exit 1
+  fi
+}
+
+main() {
+  name="$1"
+  shift
+
+  # Figure out where this thing is
+  # Assumes a directory of the form posts/year/month/date/name
+  post_dir="src/posts/$(date -j "+%Y/%m/%d")/${name}"
+
+  echo "Removing post directory at '${post_dir}'"
+  echo rm -rv "${post_dir}"; or_exit
+
+  echo 'Success.'
+  exit 0
+}
+
+main "$@"
